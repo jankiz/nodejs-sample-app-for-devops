3a44e223c29dde5eb70110b1e762994ab55b9502
Zoltan R. Janki <jankiz@inf.u-szeged.hu>
2024-11-01 01:03:42 +0100

    zabbix-agent2 start is added.

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 # Start rsyslog
 rsyslogd
 
+#Start Zabbix Agent 2
+/usr/sbin/zabbix_agent2
+
 # Start your app with PM2
 pm2-runtime start dist/index.js --output /var/log/pm2/out.log --error /var/log/pm2/error.log
