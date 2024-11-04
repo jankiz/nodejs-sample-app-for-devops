4bf8911df6cea30a3bc1aff1e078eef80a788dd1
Zoltan R. Janki <jankiz@inf.u-szeged.hu>
2024-11-04 08:48:58 +0100

    running zabbix in the background.

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ CONTAINER_NAME=$(hostname)
 # Update Zabbix agent config with the hostname
 sed -i "s/Hostname=.*/Hostname=$CONTAINER_NAME/" /etc/zabbix/zabbix_agent2.conf
 #Start Zabbix Agent 2
-/usr/sbin/zabbix_agent2
+/usr/sbin/zabbix_agent2 &
 
 # Start your app with PM2
 pm2-runtime start dist/index.js --output /var/log/pm2/out.log --error /var/log/pm2/error.log
